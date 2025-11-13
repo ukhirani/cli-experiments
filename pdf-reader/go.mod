4a59bb055adcc7278bc69a3c98c498de9ae05bb5
Umang Hirani <umanghirani.exe@gmail.com>
2025-11-13 22:04:45 +0530

    trying out the exec command approach
    using the xpdf's pdftotext command

---
+++ pdf-reader/go.mod
@@ -0,0 +1,5 @@
+module pdf-reader
+
+go 1.25.3
+
+require github.com/dslipak/pdf v0.0.2 // indirect
---
+++ pdf-reader/main.go
@@ -0,0 +1,33 @@
+// Source - https://stackoverflow.com/a
+// Posted by Zeke Lu
+// Retrieved 2025-11-13, License - CC BY-SA 4.0
+
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	// See "man pdftotext" for more options.
+	args := []string{
+		"-layout",    // Maintain (as best as possible) the original physical layout of the text.
+		"-nopgbrk",   // Don't insert page breaks (form feed characters) between pages.
+		"ddia.pdf",   // The input file.
+		"output.txt", // Send the output to stdout.
+	}
+	cmd := exec.CommandContext(context.Background(), "pdftotext", args...)
+
+	var buf bytes.Buffer
+	cmd.Stdout = &buf
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(buf.String())
+}
