68848281f8766d9cd276fa79df50cab7e70e03cd
Umang Hirani <umanghirani.exe@gmail.com>
2025-12-02 14:08:56 +0530

    list the files in the current directory

---
+++ read-write-platform-agnostic-locations/list-files.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	targetDir := "."
+	entries, err := os.ReadDir(targetDir)
+	if err != nil {
+		fmt.Printf("Error reading directory '%s': %v\n", targetDir, err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Contents of '%s':\n", targetDir)
+	for _, entry := range entries {
+		if entry.IsDir() {
+			fmt.Printf("  [DIR] %s\n", entry.Name())
+		} else {
+			fmt.Printf("  [FIL] %s\n", entry.Name())
+		}
+	}
+}
